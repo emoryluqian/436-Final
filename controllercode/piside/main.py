329aab8f69e5ab56186f77caef2d38ac42b73551
entao <entao@tamu.edu>
2021-04-18 13:11:29 -0500

    test the code

--- controllercode/piside/main.py
+++ controllercode/piside/main.py
@@ -5,40 +5,41 @@ from connection import connect_port
 
 import time
 import numpy as np
-import matplotlib.pyplot as plt
 
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 sock = connect_port()
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+# GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BCM) # Use physical pin numbering
 
 #right
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 #left
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 #up
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 #down
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 #enter
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 while True:
-    data = []
+    data = ""
     time.sleep(0.1)
-    if GPIO.input(10) == GPIO.HIGH:
-        data.append(" left")
-    if GPIO.input(10) == GPIO.HIGH:
-        data.append(" right")
-    if GPIO.input(10) == GPIO.HIGH:
-        data.append(" up")
-    if GPIO.input(10) == GPIO.HIGH:
-        data.append(" down")
-    if GPIO.input(10) == GPIO.HIGH:
-        data.append(" enter")
+    if not GPIO.input(16):
+        data += " left"
+    if not GPIO.input(17):
+        data += " right"
+    if not GPIO.input(27):
+        data += " up"
+    if not GPIO.input(22):
+        data += " down"
+    if not GPIO.input(23):
+        data += " enter"
+    # arr = (map(lambda e: bytes(e,"utf-8"), data))
+    print(data)
     sock.send(data)
 sock.close()
